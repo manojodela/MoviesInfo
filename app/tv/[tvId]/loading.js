b4105a91ec2eed78bd6654abf601b1d30c947064
Manoj Odela <manoj.odela@winfosolutions.com>
2026-01-21 21:05:55 +0530

    Revert "intial commit"
    This reverts commit ee5f8bd30b53cfb7a72947cfaf0061a23f8253c6.

--- app/tv/[tvId]/loading.js
+++
@@ -1,9 +0,0 @@
-import { Spin } from 'antd';
-
-export default function TVLoading() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
-      <Spin size="large" tip="Loading TV show details..." />
-    </div>
-  );
-}
